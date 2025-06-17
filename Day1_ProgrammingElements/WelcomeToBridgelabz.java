e3ba870be31964319f51d22deda236aec97d1780
Akshat Verma <vakshat613@gmail.com>
2025-06-17 10:27:21 +0530

    Day1 Programming Elements Solutions

--- Day1_ProgrammingElements/WelcomeToBridgelabz.java
+++
@@ -1,9 +0,0 @@
-package Day1_ProgrammingElements;
-
-import java.util.*;
-public class WelcomeToBridgelabz{
-    public static void main(String[] args) {
-        System.out.println("Welcome to Bridgelabz!");
-    }
-}
-
