ddfabf53fcacd79718d3114981757ee4eaa15cfa
Akshat Verma <vakshat613@gmail.com>
2025-06-24 21:57:02 +0530

    Level 1 Problems

---
+++ Day6_Class_and_Objects/Level1/Employee.java
@@ -0,0 +1,32 @@
+package Day6_Class_and_Objects.Level1;
+
+import java.util.*;
+
+public class Employee {
+    private String name;
+    private int id;
+    private double salary;
+
+    public Employee(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Salary: " + salary);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String name = sc.nextLine();
+        int id = sc.nextInt();
+        double salary = sc.nextDouble();
+        Employee employee = new Employee(name, id, salary);
+        employee.displayDetails();
+    }
+}
+
+
---
+++ Day6_Class_and_Objects/Level1/MobilePhone.java
@@ -0,0 +1,31 @@
+package Day6_Class_and_Objects.Level1;
+
+import java.util.*;
+
+public class MobilePhone {
+    private String brand;
+    private String model;
+    private double price;
+
+    public MobilePhone(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Model: " + model);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String brand = sc.nextLine();
+        String model = sc.nextLine();
+        double price = sc.nextDouble();
+        MobilePhone phone = new MobilePhone(brand, model, price);
+        phone.displayDetails();
+    }
+}
+
