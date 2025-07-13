20cb8e53012ffec3b69463a43a234e94a50d8c2c
Akshat Verma <vakshat613@gmail.com>
2025-07-13 10:22:56 +0530

    Akshat add. BankAccountSystem

---
+++ Day_4_final_Static_this_instance/BankAccountSystem.java
@@ -0,0 +1,39 @@
+package Day_4_final_Static_this_instance;
+
+class BankAccount {
+    static String bankName = "State Bank of India";
+    static int totalAccounts = 0;
+
+    final int accountNumber;
+    String accountHolderName;
+
+    BankAccount(String accountHolderName, int accountNumber) {
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+        totalAccounts++;
+    }
+
+    static void getTotalAccounts() {
+        System.out.println("Total Bank Accounts: " + totalAccounts);
+    }
+
+    void displayDetails() {
+        if (this instanceof BankAccount) {
+            System.out.println("Account Holder: " + accountHolderName);
+            System.out.println("Account Number: " + accountNumber);
+            System.out.println("Bank: " + bankName);
+        }
+    }
+}
+
+public class BankAccountSystem {
+    public static void main(String[] args) {
+        BankAccount acc1 = new BankAccount("Akshat", 1001);
+        BankAccount acc2 = new BankAccount("Aman", 1002);
+
+        acc1.displayDetails();
+        acc2.displayDetails();
+
+        BankAccount.getTotalAccounts();
+    }
+}
