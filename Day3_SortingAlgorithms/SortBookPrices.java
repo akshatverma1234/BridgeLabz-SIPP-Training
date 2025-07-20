3a3ae58f3d995f255cbbed25b120201e02eba5d0
akshatverma1234 <131528804+akshatverma1234@users.noreply.github.com>
2025-07-20 17:56:33 +0530

    Akshat add. BookPrices

---
+++ Day3_SortingAlgorithms/SortBookPrices.java
@@ -0,0 +1,41 @@
+package Day3_SortingAlgorithms;
+
+import java.util.*;
+
+public class SortBookPrices {
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int l, int m, int r) {
+        int[] left = Arrays.copyOfRange(arr, l, m + 1);
+        int[] right = Arrays.copyOfRange(arr, m + 1, r + 1);
+
+        int i = 0, j = 0, k = l;
+        while (i < left.length && j < right.length) {
+            arr[k++] = (left[i] <= right[j]) ? left[i++] : right[j++];
+        }
+        while (i < left.length) arr[k++] = left[i++];
+        while (j < right.length) arr[k++] = right[j++];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of books: ");
+        int n = sc.nextInt();
+        int[] prices = new int[n];
+
+        System.out.println("Enter book prices:");
+        for (int i = 0; i < n; i++){
+             prices[i] = sc.nextInt();
+        }
+        mergeSort(prices, 0, n - 1);
+        System.out.println("Sorted Book Prices:");
+        System.out.println(Arrays.toString(prices));
+    }
+}
