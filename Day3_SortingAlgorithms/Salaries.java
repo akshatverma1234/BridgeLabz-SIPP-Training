460c9e628a1dc1f05127d6a47296a04779de6fb9
akshatverma1234 <131528804+akshatverma1234@users.noreply.github.com>
2025-07-20 17:57:59 +0530

    Akshat add. Salaries

---
+++ Day3_SortingAlgorithms/Salaries.java
@@ -0,0 +1,35 @@
+package Day3_SortingAlgorithms;
+
+import java.util.*;
+
+public class Salaries {
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i, l = 2 * i + 1, r = 2 * i + 2;
+        if (l < n && arr[l] > arr[largest]) largest = l;
+        if (r < n && arr[r] > arr[largest]) largest = r;
+        if (largest != i) {
+            int temp = arr[i]; arr[i] = arr[largest]; arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) heapify(arr, n, i);
+        for (int i = n - 1; i > 0; i--) {
+            int tmp = arr[0]; arr[0] = arr[i]; arr[i] = tmp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of applicants: ");
+        int n = sc.nextInt();
+        int[] salaries = new int[n];
+        System.out.println("Enter expected salaries:");
+        for (int i = 0; i < n; i++) salaries[i] = sc.nextInt();
+        heapSort(salaries);
+        System.out.println("Sorted Salaries:");
+        System.out.println(Arrays.toString(salaries));
+    }
+}
