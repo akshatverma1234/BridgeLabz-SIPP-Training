c7194d8724464c7d470dcc3bbe5cab3b1658ea05
akshatverma1234 <131528804+akshatverma1234@users.noreply.github.com>
2025-08-04 11:15:55 +0530

    Akshat add. CheckEqualSets

---
+++ Collections/CheckEqualSets.java
@@ -0,0 +1,25 @@
+package Generics.Collections;
+
+import java.util.*;
+
+public class CheckEqualSets {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        System.out.println("Enter size of Set1:");
+        int n1 = sc.nextInt();
+        System.out.println("Enter elements of Set1:");
+        for(int i=0;i<n1;i++) {
+            set1.add(sc.nextInt());
+        }
+        System.out.println("Enter size of Set2:");
+        int n2 = sc.nextInt();
+        System.out.println("Enter elements of Set2:");
+        for(int i=0;i<n2;i++){ 
+            set2.add(sc.nextInt());
+        }
+        System.out.println("Sets are equal: " + set1.equals(set2));
+    }
+}
+
