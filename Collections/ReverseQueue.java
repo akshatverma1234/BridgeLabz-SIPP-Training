f0ecdbe379cd9d4fc4f80af6a0bf54186d804dfc
akshatverma1234 <131528804+akshatverma1234@users.noreply.github.com>
2025-08-04 11:33:00 +0530

    Akshat add. ReverseQueue

---
+++ Collections/ReverseQueue.java
@@ -0,0 +1,22 @@
+package Generics.Collections;
+
+import java.util.*;
+
+public class ReverseQueue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<>();
+        System.out.println("Enter size:");
+
+        int n = sc.nextInt();
+        System.out.println("Enter elements:");
+
+        for(int i=0;i<n;i++){ 
+            queue.add(sc.nextInt());
+        }
+        Stack<Integer> stack = new Stack<>();
+        while(!queue.isEmpty()) stack.push(queue.poll());
+        while(!stack.isEmpty()) queue.add(stack.pop());
+        System.out.println("Reversed Queue: " + queue);
+    }
+}
