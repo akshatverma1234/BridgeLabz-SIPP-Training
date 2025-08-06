17a1b7d0a79855ef9b44ef6166d2bd5cfddc5757
akshatverma1234 <131528804+akshatverma1234@users.noreply.github.com>
2025-08-06 10:39:00 +0530

    Akshat add. PipedCommunication.

---
+++ Streams/PipedCommunication.java
@@ -0,0 +1,30 @@
+package Generics.Streams;
+
+import java.io.*;
+public class PipedCommunication {
+    public static void main(String[] args) throws IOException {
+        PipedOutputStream pos = new PipedOutputStream();
+        PipedInputStream pis = new PipedInputStream(pos);
+
+        Thread writer = new Thread(() -> {
+            try {
+                pos.write("Hello from Thread".getBytes());
+                pos.close();
+            } catch (IOException e) { e.printStackTrace(); }
+        });
+
+        Thread reader = new Thread(() -> {
+            try {
+                int data;
+                while ((data = pis.read()) != -1) {
+                    System.out.print((char) data);
+                }
+                pis.close();
+            } catch (IOException e) { e.printStackTrace(); }
+        });
+
+        writer.start();
+        reader.start();
+    }
+}
+
